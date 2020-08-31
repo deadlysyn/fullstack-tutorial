33e4ff4dc1dc666d3b0d1db0cbff07fb839e2503
Mike Hoskins <mrh@devopsdreams.io>
2020-08-31 00:16:14 -0400

    add apollo; cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import { MikroORM } from "@mikro-orm/core";
+import "reflect-metadata";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 
 const main = async () => {
   // connect to database
   const orm = await MikroORM.init(microConfig);
   // run migrations
   await orm.getMigrator().up();
-  // run sql
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
